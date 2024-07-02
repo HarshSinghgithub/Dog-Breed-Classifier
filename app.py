92b4dc9e4c2d4b39e6d819f66f7a9da79e79736a
HarshSinghgithub <97045139+HarshSinghgithub@users.noreply.github.com>
2024-07-02 20:41:59 +0530

    Created Streamlit app

--- app.py
+++ app.py
@@ -13,32 +13,8 @@ model = tf.keras.models.load_model(model_path)
 with open('label_map.json', 'r') as f:
     label_map = json.load(f)
 
-# def predict(url, true_label):
-#     response = requests.get(url)
-#     response_status = response.status_code
-
-#     if response_status == 200:
-#         img = Image.open(BytesIO(response.content))
-#         img = np.array(img)
-#         img = np.expand_dims(img, axis=0)
-
-#         pred_label = model.predict(img)
-#         pred_label = np.argmax(pred_label, axis=1)
-#         pred_label = label_map[str(pred_label[0])]
-
-#         plt.imshow(img[0])
-#         plt.title(f'True Label : {true_label} Pred Label : {pred_label}')
-#         plt.axis('off')
-#         plt.show()
-#     else:
-#         print("Failed to fetch the image.")
-
-
-
 
 if __name__ == "__main__":
-    # predict("https://hips.hearstapps.com/hmg-prod/images/wolf-dog-breeds-siberian-husky-1570411330.jpg?crop=1xw:0.84375xh;center,top", "Siberian Husky")
-    # st.title("Dog Breed Classifier")
     st.set_page_config(page_title="Dog Breed Classifier", layout='wide')
 
     uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
@@ -58,8 +34,3 @@ if __name__ == "__main__":
                 st.success(f"Prediction: {breed}")
         else:
             st.empty()
-        # st.write("Classifying")
-        # breed = model.predict(image)
-        # breed = np.argmax(breed, axis=1)
-        # breed = label_map[str(breed[0])]
-        # st.markdown( """<h2 style="text-align: center;">{}</h2> """.format(breed), unsafe_allow_html=True)
